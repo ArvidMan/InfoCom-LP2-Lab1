7061e94ae2603bf3b535c1329eec905b3e6114d6
Arvid <ar3126ma-s@student.lu.se>
2023-11-13 18:42:07 +0100

    sensehat joystick controller

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -2,7 +2,44 @@ import requests
 import time
 import random
 import click
+from sense_hat import SenseHat
+sense = SenseHat()
 
+def joy_direction():
+    d_long = 0
+    d_la = 0
+    send_vel = False
+    while True:
+        for event in sense.stick.get_events():
+            if event.action == "pressed":
+                if event.direction =="up":
+                    click.echo('Up')
+                    send_vel = True
+                    d_long = 0
+                    d_la = 1   
+                elif event.direction == "down":
+                    click.echo('Up')
+                    send_vel = True
+                    d_long = 0
+                    d_la = 1    
+                elif event.direction == "left":
+                    click.echo('Left')
+                    send_vel = True
+                    d_long = -1
+                    d_la = 0
+                elif event.direction == "right":
+                    click.echo('Right')
+                    send_vel = True
+                    d_long = 1
+                    d_la = 0
+                else:
+                    d_long = 0
+                    d_la = 0
+                    click.echo('Invalid input :(')
+                    send_vel = False
+                return d_long, d_la, send_vel
+    
+                    
 
 def get_direction():
     d_long = 0
@@ -40,7 +77,7 @@ def get_direction():
 if __name__ == "__main__":
     SERVER_URL = "http://127.0.0.1:5000/drone"
     while True:
-        d_long, d_la, send_vel = get_direction()
+        d_long, d_la, send_vel = joy_direction()
         if send_vel:
             with requests.Session() as session:
                 current_location = {'longitude': d_long,
