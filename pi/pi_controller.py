9401e275a1f77712a1014d77124c9fb8d2defc14
Arvid <ar3126ma-s@student.lu.se>
2023-11-19 16:05:58 +0100

    better get_direction

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -45,39 +45,40 @@ def get_direction():
     d_long = 0
     d_la = 0
     send_vel = False
-    c = click.getchar()
-    if c =='a':
-        click.echo('Left')
-        send_vel = True
-        d_long = -1
-        d_la = 0
-    elif c == 'd':
-        click.echo('Right')
-        send_vel = True
-        d_long = 1
-        d_la = 0
-    elif c =='w':
-        click.echo('Up')
-        send_vel = True
-        d_long = 0
-        d_la = 1
-    elif c == 's':
-        click.echo('Down')
-        send_vel = True
-        d_long = 0
-        d_la = -1
-    else:
-        d_long = 0
-        d_la = 0
-        click.echo('Invalid input :(')
-        send_vel = False
+    for event in sense.stick.get_events(): 
+        c = event.direction
+        if c =="left":
+            click.echo('Left')
+            send_vel = True
+            d_long = -1
+            d_la = 0
+        elif c == "right":
+            click.echo('Right')
+            send_vel = True
+            d_long = 1
+            d_la = 0
+        elif c =="up":
+            click.echo('Up')
+            send_vel = True
+            d_long = 0
+            d_la = 1
+        elif c == "down":
+            click.echo('Down')
+            send_vel = True
+            d_long = 0
+            d_la = -1
+        else:
+            d_long = 0
+            d_la = 0
+            click.echo('Invalid input :(')
+            send_vel = False
     return d_long, d_la, send_vel
 
 
 if __name__ == "__main__":
     SERVER_URL = "http://127.0.0.1:5000/drone"
     while True:
-        d_long, d_la, send_vel = joy_direction()
+        d_long, d_la, send_vel = get_direction()
         if send_vel:
             with requests.Session() as session:
                 current_location = {'longitude': d_long,
